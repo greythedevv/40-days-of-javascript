987a1ae10eafabf30eaf337f789169620547effc
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-02-12 23:33:13 +0100

    progress

---
+++ Day13/day13.js
@@ -0,0 +1,23 @@
+console.log("Day 13 JS file is loaded"); 
+
+
+let tom = {
+    name: "tom",
+    age: 17
+}
+
+let jerry = {
+    name: "jerry",
+    age: 18
+}
+
+function greetMe(obj){
+    obj.logMessage = function (){
+        console.log(`${this.name} is ${this.age} years old!`)
+    }
+    console.log(obj)
+}
+
+greetMe(tom);
+tom.logMessage()
+console.log(this.name)
