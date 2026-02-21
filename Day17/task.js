38a43e1d1e5a6e9ddf2a31bcfba1513f9d6f5939
Greatness <Oluwolegreatnessadeola@gmail.com>
2026-02-21 23:23:54 +0100

    Day 17 Introduction to DM

---
+++ Day17/task.js
@@ -0,0 +1,12 @@
+// 1. Find the Most Frequent Word in a Paragraph
+// Consider the follwoing HTML:
+
+// <div id="text">This is a test. This test is only a test.</div>
+// Now, find and display the most frequently occurring word. Also put a count of occurance beside it.
+
+// Hints:
+
+// Use document.querySelector() or getElementById() to select the paragraph.
+// Convert the text into an array of words.
+// Use querySelector() to display the most frequent word along with the count inside another <div>.
+
